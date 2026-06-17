116dce32c3fad5af5cab73156a1a236f1b4c3f9b
visheshbpatel <iamvisheshpatel@gmail.com>
2026-06-17 21:30:52 +0530

    add data type validation

--- 01-why-pandatic/01-why-pandatic.py
+++ 01-why-pandatic/01-why-pandatic.py
@@ -1,7 +1,27 @@
-def insert_patient_data(name, age):
+def insert_patient_data(name: str, age: int):
 
-    print(name)
-    print(age)
-    print("Inserted into database")  # just for testing
+    if type(name) == str and type(age) == int:
 
-insert_patient_data("vbp","twenty two")
+        if age<0:
+            raise ValueError("Age can't be negative")
+        
+        print(name)
+        print(age)
+        print("Inserted into database")  # just for testing
+    else:
+        raise TypeError("Incorrect Datatype")
+    
+
+def update_patient_data(name: str, age: int):
+
+    if type(name) == str and type(age) == int:
+        print(name)
+        print(age)
+        print("Updated into database")
+    else:
+        raise TypeError("Incorrect Datatype")
+
+
+
+insert_patient_data("vbp", 8)
+
