11135ea14bd9a344b5fca337788cc1d15663d7c9
visheshbpatel <iamvisheshpatel@gmail.com>
2026-06-18 11:53:15 +0530

    fix: correct pydantic spelling in folder and file names

--- 01-why-pydantic/01-why-pydantic.py
+++ 01-why-pydantic/01-why-pydantic.py
@@ -6,12 +6,12 @@ class Patient(BaseModel):
 
     name: Annotated[str, Field(max_length=50, title="Name of the Patient", description="Give the name of the patient in less than 50 characters", examples=['Vishesh','Madhav'])]
     email: EmailStr
-    github_url: AnyUrl
+    hospital_url: AnyUrl
     age: int = Field(gt=0, lt=120)
     weight: Annotated[float, Field(gt=0, strict=True)]
-    married: Annotated[bool, Field(default=None, description="Is the person Married or not")]
+    married: Annotated[Optional[bool], Field(default=None, description="Is the person Married or not")]
     allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
-    contact_details: Dict[str,int]
+    contact_details: Dict[str,str]
 
 
 def insert_patient_data(patient: Patient):
@@ -40,13 +40,11 @@ def update_patient_data(patient: Patient):
     print("Update into database")  # just for testing
 
 
-patient1_info = {"name":"vbp", "email":"vbp@gmail.com", "github_url":"https://github.com/visheshbpatel", "age":22, "weight":65,  'contact_details':{'phone':789789}}
-# we already se married to false
-# allergies was not added because it is option and set as None
+patient1_info = {"name":"vbp", "email":"vbp@gmail.com", "hospital_website": "https://www.apollohospitals.com", "age":22, "weight":65,  'contact_details':{'phone':"789789"}}
+# married was not provided, so it defaults to None
+# # allergies was not added because it is option and set as None
 
 patient1 = Patient(**patient1_info)
 
 insert_patient_data(patient1)
-
-
 
