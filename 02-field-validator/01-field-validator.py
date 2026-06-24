cdf4f06c028a22b509cdf539e4f8054373e91342
visheshbpatel <iamvisheshpatel@gmail.com>
2026-06-24 17:47:21 +0530

    refactor: rename github_url field to hospital_url

--- 02-field-validator/01-field-validator.py
+++ 02-field-validator/01-field-validator.py
@@ -6,7 +6,7 @@ class Patient(BaseModel):
 
     name: Annotated[str, Field(max_length=50, title="Name of the Patient", description="Give the name of the patient in less than 50 characters", examples=['Vishesh','Madhav'])]
     email: EmailStr
-    github_url: AnyUrl
+    hospital_url: AnyUrl
     age: int = Field(gt=0, lt=120)
     weight: Annotated[float, Field(gt=0, strict=True)]
     married: Annotated[bool, Field(default=None, description="Is the person Married or not")]
@@ -67,7 +67,7 @@ def update_patient_data(patient: Patient):
     print("Update into database")  # just for testing
 
 
-patient1_info = {"name":"vbp", "email":"vbp@hdfc.com", "github_url":"https://github.com/visheshbpatel", "age":"22", "weight":65,  'contact_details':{'phone':789789}}
+patient1_info = {"name":"vbp", "email":"vbp@hdfc.com", "hospital_url": "https://www.apollohospitals.com", "age":"22", "weight":65,  'contact_details':{'phone':789789}}
 
 
 patient1 = Patient(**patient1_info)    # validation perfomerd here  --> type coercion
