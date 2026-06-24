a546c2cbeb474e0d22aa8721f8c3f95c7eabc810
visheshbpatel <iamvisheshpatel@gmail.com>
2026-06-24 17:46:00 +0530

    Update model validator and hospital URL field

--- 03-model-validator/01-model-validator.py
+++ 03-model-validator/01-model-validator.py
@@ -6,7 +6,7 @@ class Patient(BaseModel):
 
     name: Annotated[str, Field(max_length=50, title="Name of the Patient", description="Give the name of the patient in less than 50 characters", examples=['Vishesh','Madhav'])]
     email: EmailStr
-    github_url: AnyUrl
+    hospital_url: AnyUrl
     age: int = Field(gt=0, lt=120)
     weight: Annotated[float, Field(gt=0, strict=True)]
     married: Annotated[bool, Field(default=None, description="Is the person Married or not")]
@@ -42,10 +42,10 @@ class Patient(BaseModel):
         
 
     @model_validator(mode='after')
-    def validate_emergency_contact(cls, model):
-        if model.age > 60 and 'emergency' not in model.contact_details:
+    def validate_emergency_contact(self):
+        if self.age > 60 and 'emergency' not in self.contact_details:
             raise ValueError("Patients older than 60 must have an emergency contanct")
-        return model
+        return self
 
 
 def insert_patient_data(patient: Patient):
@@ -74,7 +74,7 @@ def update_patient_data(patient: Patient):
     print("Update into database")  # just for testing
 
 
-patient1_info = {"name":"vbp", "email":"vbp@hdfc.com", "github_url":"https://github.com/visheshbpatel", "age":"65", "weight":65,  'contact_details':{'phone':789789, 'emergency':898989}}
+patient1_info = {"name":"vbp", "email":"vbp@hdfc.com", "hospital_url": "https://www.apollohospitals.com", "age":"65", "weight":65,  'contact_details':{'phone':789789, 'emergency':898989}}
 
 
 patient1 = Patient(**patient1_info)    # validation perfomerd here  --> type coercion
